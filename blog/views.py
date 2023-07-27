9015a2b1693bcd850d8645f5759a01106a985bcf
CamilleForini <camillebatista54@gmail.com>
2023-07-27 16:52:38 -0300

    templates and static folders created

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def blog(request):
     ...
-    return HttpResponse("blog")
+    return render(request, "blog/index.html")
--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def home(request):
-    print("")
-    return HttpResponse("Home page")
+    print("home")
+    return render(request, "home/index.html")
