b1906d5d4dd4197f2f6bdbdfcd9d87620b2b5007
CamilleForini <camillebatista54@gmail.com>
2023-08-07 15:22:54 -0300

    data.py e laço for

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
+from blog.data import posts
 
 
 # Create your views here.
 def blog(request):
-    contexto = {"text": "Estamos no blog", "title": "Blog - "}
+    contexto = {
+        "text": "Estamos no blog",
+        "title": "Blog - ",
+        "posts": posts,
+    }
     return render(request, "blog/index.html", contexto)
 
 
 def exemplo(request):
     print("exemplo")
-    contexto = {"text": "Estamos no exemplo do blog", "title": "Exemplo - "}
+    contexto = {
+        "text": "Estamos no exemplo do blog",
+        "title": "Exemplo - ",
+    }
 
     return render(request, "blog/exemplo.html", contexto)
